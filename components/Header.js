52ebb5997f10d53987aa10f914b5f82e809a47e5
Evan Prenger <eprenger23@gmail.com>
2021-12-09 22:05:31 -0500

    update

---
+++ components/Header.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <div>
+      {/* left */}
+      <div>
+        <Image src="https://links.papareact.com/ocw" layout="fill" />
+      </div>
+
+      {/* middle */}
+      {/* right */}
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,18 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
-        <title>Create Next App</title>
+        <title>Instagram Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Instagram 2.0 Build</h1>
+      {/* header */}
+      <Header />
+      {/* feed */}
+      {/* modal */}
     </div>
   );
 }
