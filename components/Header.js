2729757cddd082c76c0ffab720c81763e9c27199
Evan Prenger <eprenger23@gmail.com>
2021-12-09 22:28:28 -0500

    commit

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,48 @@
 import Image from "next/image";
 
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from "@heroicons/react/outline";
+
 export default function Header() {
   return (
     <div>
-      {/* left */}
-      <div>
-        <Image src="https://links.papareact.com/ocw" layout="fill" />
-      </div>
+      <div className="flex justify-between max-w-6xl">
+        {/* left */}
+        <div className="relative hidden lg:inline-grid w-24">
+          <Image
+            src="https://links.papareact.com/ocw"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <div className="relative w-10 flex-shrink-0 lg:hidden cursor-pointer ">
+          <Image
+            src="https://links.papareact.com/jjm"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
 
-      {/* middle */}
-      {/* right */}
+        {/* middle - search input */}
+        <div className="mt-1 relative p-3 rounded-md ">
+          <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+            <SearchIcon className="h-5 w-5 text-gray-500" />
+          </div>
+          <input
+            className="bg-gray-50 block w-full pl-10 sm:text-sm 
+            border-gray-300  focus:ring-black focus:border-black rounded-md"
+            placeholder="Search"
+          />
+        </div>
+
+        {/* right */}
+      </div>
     </div>
   );
 }
