51afb7c9c6291d03d04f3886744ff8725e4d14bb
Evan Prenger <eprenger23@gmail.com>
2021-12-10 13:35:08 -0500

    finish header

--- components/Header.js
+++ components/Header.js
@@ -12,7 +12,7 @@ import {
 
 export default function Header() {
   return (
-    <div>
+    <div className="shadow-sm border-b bg-white sticky top-0 z-50">
       <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
         {/* left */}
         <div className="relative hidden lg:inline-grid w-24">
@@ -52,7 +52,23 @@ export default function Header() {
         <div className="flex items-center justify-end space-x-4">
           <HomeIcon className=" navBtn" />
           <MenuIcon className="md:hidden h-6 cursor-pointer" />
-          <PaperAirplaneIcon className="navBtn" />
+          <div className="relative navBtn">
+            <PaperAirplaneIcon className="navBtn rotate-45" />
+            <div
+              className="absolute -top-2 -right-3 bg-blue-400 rounded-full flex 
+               text-white items-center justify-center text-xs w-5 h-5"
+            >
+              3
+            </div>
+          </div>
+          <PlusCircleIcon className="navBtn" />
+          <UserGroupIcon className="navBtn" />
+          <HeartIcon className="navBtn" />
+          <img
+            src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+            alt="profile pic"
+            className="h-10 rounded-full cursor-pointer"
+          />
         </div>
       </div>
     </div>
