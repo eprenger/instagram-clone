9faef95137f1870e6f1f6860875c5c0fa4079c72
Evan Prenger <eprenger23@gmail.com>
2021-12-12 20:29:16 -0500

    updates

--- components/Story.js
+++ components/Story.js
@@ -1,5 +1,14 @@
-function Story() {
-  return <div></div>;
+function Story({ img, username }) {
+  return (
+    <div>
+      <img
+        className="h-14 w-14 rounded-full p-[1.5px] border-2 border-red-500 cursor-pointer"
+        src={img}
+        alt=""
+      />
+      <p>{username}</p>
+    </div>
+  );
 }
 
 export default Story;
