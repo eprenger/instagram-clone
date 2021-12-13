8bb585a0a7d38591b68e6afd51de40d2f58eed32
Evan Prenger <eprenger23@gmail.com>
2021-12-13 15:49:43 -0500

    init nextauth / firebase

---
+++ firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFireStore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB14hrDFPEy888jWssuX9q3WA2pJwgTUqU",
+  authDomain: "instagram-clone-4228f.firebaseapp.com",
+  projectId: "instagram-clone-4228f",
+  storageBucket: "instagram-clone-4228f.appspot.com",
+  messagingSenderId: "969819667249",
+  appId: "1:969819667249:web:2610d9f5ea4c801b4b0e10",
+};
+
+// Initialize Firebase
+const app = !getApp().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirebase();
+const storage = getStorage();
+
+export { app, db, storage };
