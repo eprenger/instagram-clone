44d574f4bac5f740a4ed7128e193ced6ab66c506
Evan Prenger <eprenger23@gmail.com>
2021-12-12 21:45:26 -0500

    stories section

--- components/Story.js
+++ components/Story.js
@@ -2,11 +2,12 @@ function Story({ img, username }) {
   return (
     <div>
       <img
-        className="h-14 w-14 rounded-full p-[1.5px] border-2 border-red-500 cursor-pointer"
+        className="h-14 w-14 rounded-full p-[1.5px] border-2 border-red-500 cursor-pointer
+        hover:scale-110 transition transform duration-200 ease-out"
         src={img}
         alt=""
       />
-      <p>{username}</p>
+      <p className="text-xs w-14 truncate text-center">{username}</p>
     </div>
   );
 }
