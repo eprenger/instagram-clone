7e6fc7cc91671cf0e6ae84ea9ad493cffc0689cc
Evan Prenger <eprenger23@gmail.com>
2021-12-12 20:29:03 -0500

    updates

--- components/Stories.js
+++ components/Stories.js
@@ -15,9 +15,16 @@ function Stories() {
   }, []);
 
   return (
-    <div>
+    <div
+      className="flex space-x-2 p-6 bg-white mt-8
+    border border-gray-200 rounded-sm overflow-x-scroll"
+    >
       {suggestions.map((profile) => (
-        <Story key={profile.id} />
+        <Story
+          key={profile.id}
+          img={profile.avatar}
+          username={profile.username}
+        />
       ))}
       {/* stories */}
       {/* stories */}
